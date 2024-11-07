702b68f47e188f2c6c2aa5ee5b930c20ebe8aee4
ramuk1107 <kumar11sathish@gmail.com>
2024-11-07 19:44:48 +0530

    Updated the code to use env file

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,76 +1,81 @@
-"use client"
+"use client";
+import AppointmentCard from "../components/dashboard-card/patient-appointment-card";
+import ProviderAppointmentCard from "../components/dashboard-card/provider-appointment-card";
 
 import LeftMenu from "../components/ui-utils/left-menu";
 import classes from "./page.module.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Dashbaord() {
-    const [userData, setUserData] = useState(null);
-    const [appointmentData, setAppointmentData] = useState(null);
-    const token = localStorage.getItem('token');
+  const [userData, setUserData] = useState(null);
+  const [appointmentData, setAppointmentData] = useState(null);
+  const token = localStorage.getItem("token");
 
-    useEffect(() => {
-        try {
-            async function fetchPatients() {
-                const url = 'http://10.137.203.231:3456/api/v0/user';
+  useEffect(() => {
+    try {
+      async function fetchPatients() {
+        const url = process.env.NEXT_PUBLIC_BACKEND_BASE_URL + "/api/v0/user";
 
-                fetch(url, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": `Bearer ${token}`, // Include the token here
-                    },
-                    // credentials: 'include', // Important: include cookies in request
-                    method: 'Get',
-                })
-                .then((response) => response.json())
-                .then((data) => {
-                    // Handle server response
-                    console.log("===== fetchUsers =====",data);
-                    const userData = data['data']
-                    setUserData(userData)
-                    // console.log("===== fetchUsers =====",setUserData)
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-            }
-            
-            async function fetchAppointments() {
-                const url = 'http://10.137.203.231:3456/api/v0/appointment';
+        fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // Include the token here
+          },
+          // credentials: 'include', // Important: include cookies in request
+          method: "Get",
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // Handle server response
+            console.log("===== fetchUsers =====", data);
+            const userData = data["data"];
+            setUserData(userData);
+            // console.log("===== fetchUsers =====",setUserData)
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
 
-                fetch(url, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": `Bearer ${token}`, // Include the token here
-                    },
-                    // credentials: 'include', // Important: include cookies in request
-                    method: 'Get',
-                })
-                .then((response) => response.json())
-                .then((data) => {
-                    // Handle server response
-                    console.log("===== fetchAppointments =====",data);
-                    const appointmentData = data['data']
-                    setAppointmentData(appointmentData)
-                    // console.log("===== fetchAppointments =====",setUserData)
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-            }
+      async function fetchAppointments() {
+        const url =
+          process.env.NEXT_PUBLIC_BACKEND_BASE_URL + "/api/v0/appointment";
 
-            fetchPatients()
-            fetchAppointments()
-        } catch (error) {
-            console.error("handleSubmit exception error:", error);
-        }
-    }, [])
+        fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // Include the token here
+          },
+          // credentials: 'include', // Important: include cookies in request
+          method: "Get",
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // Handle server response
+            console.log("===== fetchAppointments =====", data);
+            const appointmentData = data["data"];
+            setAppointmentData(appointmentData);
+            // console.log("===== fetchAppointments =====",setUserData)
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+
+      fetchPatients();
+      fetchAppointments();
+    } catch (error) {
+      console.error("handleSubmit exception error:", error);
+    }
+  }, []);
 
   return (
     <div className={classes.container}>
       <LeftMenu></LeftMenu>
       <div className={classes.mainContnet}>
         <h1 className={classes.welcomeMsg}>Welcome, Dr. Smith </h1>
+        <AppointmentCard></AppointmentCard>
+        <ProviderAppointmentCard></ProviderAppointmentCard>
       </div>
     </div>
   );

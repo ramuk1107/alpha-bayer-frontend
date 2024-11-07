ac936d8af1d08683ad4bcfaeaaf9ecf16f4a70fe
ramuk1107 <kumar11sathish@gmail.com>
2024-11-07 15:33:13 +0530

    Optimised the home page code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 import classes from "./page.module.css";
-import TopicCard from "./components/ui-utils/card";
-import HealthTips from "./health-tips/page";
 import HealthTipsComp from "./components/health-tips/health-tips";
 import HealthNewsComp from "./components/health-news/health-news";
+
+const NAV_CONSTANT = [
+  "Home",
+  "Health Topics",
+  "Resources",
+  "About Us",
+  "Contact",
+];
 
 export default function Home() {
   return (
@@ -13,21 +17,11 @@ export default function Home() {
       <div className={classes.headerCont}>
         <h1 className={classes.title}>Bayer Healthcare</h1>
         <div className={classes.navBg}>
-          <Link className={classes.navLink} href={"/"}>
-            Home
-          </Link>
-          <Link className={classes.navLink} href={"/"}>
-            Health Topics
-          </Link>
-          <Link className={classes.navLink} href={"/"}>
-            Resources
-          </Link>
-          <Link className={classes.navLink} href={"/"}>
-            About Us
-          </Link>
-          <Link className={classes.navLink} href={"/"}>
-            Contact
-          </Link>
+          {NAV_CONSTANT.map((nav, index) => (
+            <Link className={classes.navLink} href={"/"}>
+              {nav}
+            </Link>
+          ))}
         </div>
         <h1 className={classes.subTitle}>Your Health, Our Priority</h1>
         <p className={classes.subDesc}>

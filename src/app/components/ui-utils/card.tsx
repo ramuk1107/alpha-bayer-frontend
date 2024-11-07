17de2b5b2f91be7596cd5d10683888bd457cc0e0
ramuk1107 <kumar11sathish@gmail.com>
2024-11-07 15:23:28 +0530

    Added Component comments

--- src/app/components/ui-utils/card.tsx
+++ src/app/components/ui-utils/card.tsx
@@ -2,9 +2,9 @@ import React, { ReactNode } from "react";
 import classes from "./card.module.css";
 
 interface TopicCardProps {
-  title: string;
-  description: string;
-  btnTitle: string;
+  title: string; // Title to be displayed in the card
+  description: string; // Description/content of the card
+  btnTitle: string; // Text to be displayed on the button
 }
 
 const TopicCard: React.FC<TopicCardProps> = ({
@@ -12,13 +12,20 @@ const TopicCard: React.FC<TopicCardProps> = ({
   description,
   btnTitle,
 }) => {
+  // Render the card structure
   return (
     <div className={classes.cardBg}>
+      {/* Card title */}
       <h3>{title}</h3>
+
+      {/* Card description/content */}
       <p>{description}</p>
+
+      {/* Action button */}
       <button>{btnTitle}</button>
     </div>
   );
 };
 
+// Export the component as default for importing in other files
 export default TopicCard;

10a097cfc5a3c31fe97b60a496745b0fa2ea59d7
rldarshan <rl.darshan01@gmail.com>
2024-11-07 16:19:22 +0530

    Added login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+// import { hydrateRoot } from 'react-dom/client';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
